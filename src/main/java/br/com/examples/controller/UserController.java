515372bb47cc15b0decce310b4b66f74b309198a
Davi Nascimento da Silva <davinascimentodasilva10@gmail.com>
2025-07-07 13:29:03 -0300

    Criacao Endpoints Create Update and Delete User

--- src/main/java/br/com/examples/controller/UserController.java
+++ src/main/java/br/com/examples/controller/UserController.java
@@ -2,10 +2,14 @@ package br.com.examples.controller;
 
 import br.com.examples.model.User;
 import br.com.examples.repository.UserRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(value = "http://localhost:4200")
 @RestController
@@ -16,15 +20,41 @@ public class UserController {
     private UserRepository userRepository;
 
     @GetMapping
+    @ResponseStatus(value = HttpStatus.OK)
     public List<User> getUsers() {
         return userRepository.findAll();
     }
 
     @GetMapping(value = "/{id}")
-    public User getUser(@PathVariable Long id) {
-        return userRepository.findById(id).orElseThrow(
-                () -> new RuntimeException("User not found.")
-        );
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        Optional<User> user = userRepository.findById(id);
+        return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User save = userRepository.save(user);
+        return ResponseEntity.status(201).body(save);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
+        return userRepository.findById(id)
+                .map(existingUser -> {
+                    BeanUtils.copyProperties(user, existingUser, "id");
+                    return ResponseEntity.ok(userRepository.save(existingUser));
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable Long id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            userRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 
 }
